d6a90d3d3cbc59466db4b4b629e1305056ce9ec7
lisalee202 <leehyunjung139@gmail.com>
2024-07-23 17:59:00 +0900

    JS - Header

---
+++ src/main2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//Make header transparent when it is on the top
+const header = document.querySelector(".header");
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header--dark");
+  } else {
+    header.classList.remove("header--dark");
+  }
+});
