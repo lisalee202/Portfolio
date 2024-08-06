2df19600c8250a8bb5a9ec201fc42a3acc6580b2
lisalee202 <leehyunjung139@gmail.com>
2024-08-06 15:05:57 +0900

    JS - Home

--- src/main2.js
+++ src/main2.js
@@ -10,3 +10,10 @@ document.addEventListener("scroll", () => {
     header.classList.remove("header--dark");
   }
 });
+
+//Home 섹션을 스크롤시 투명하게 처리함
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  console.log(1 - window.scrollY / homeHeight);
+});
