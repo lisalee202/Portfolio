c14ae25d3c25e826198339da60daceaeaf84dd65
lisalee202 <leehyunjung139@gmail.com>
2024-10-08 10:25:18 +0900

    JS - Menu

--- src/main2.js
+++ src/main2.js
@@ -27,3 +27,15 @@ document.addEventListener("scroll", () => {
     arrowUp.style.opacity = 0;
   }
 });
+
+// Navbar toggle btn 클릭 처리
+const navbarMenu = document.querySelector(".header__menu");
+const navbarToggle = document.querySelector(".header__toggle");
+navbarToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+// Navbar menu 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener("click", () => {
+  navbarMenu.classList.remove("open");
+});
